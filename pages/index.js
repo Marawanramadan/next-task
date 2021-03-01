34b69f294aba6d1975287dcef772dba6f27c8dd1
Marawanramadan <marawan_engineer2013@yahoo.com>
2021-03-01 21:40:27 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,81 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+const Home =(props) => {
+  
+  // async componentDidMount() {
+  //   const result = await axios.get(
+  //     `http://80.240.21.204:1337/news?skip=8&limit=5`
+  //   );
+  //   console.log(result.data.news);
+  //   this.setState({ news: result.data.news });
+  // }
+//  loadNext = async ()=>{
+//     const result = await axios.get(
+//       `http://80.240.21.204:1337/news?skip=8&limit=5`
+//     );
+//     console.log(result.data.news);
+//     this.setState(prevState=> {
+//     return {...prevState,news: prevState.news.concat(result.data.news) }})
+// }
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+  
+    console.log(props.props.news)
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col lg={4}>
+              <h1>first column</h1>
+            </Col>
+            <Col lg={4}>
+            {/* <InfiniteScroll
+                dataLength={props.news.length}
+                hasMore={true}
+                next={this.loadNext}
+                loader={
+                  <div className="loader" key={0}>
+                    Loading ...
+                  </div>
+                }
+                endMessage={
+                  <p style={{ textAlign: "center" }}>
+                    <b>Yay! You have seen it all</b>
+                  </p>
+                }
+              >*/}
+                {
+                  
+                  props.props.news.map((info) => {
+                  return (
+                    <div key={info._id}>
+                      <h1>{info.source.title}</h1>
+                      <img src={info.source.url} />
+                    </div>
+                  );
+                }) 
+                }
+              {/* </InfiniteScroll>  */}
+            </Col>
+            <Col lg={4}>third column</Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+Home.getInitialProps = async (ctx) =>{
+  const results = await axios.get(`http://80.240.21.204:1337/news?skip=8&limit=5`)
+  const news = results.data.news
+  return {
+    props : {news}
+  }
 }
+export default Home;
